25cc58cbe91daf637c45e1af5b76c2870a39d156
jneeb <neebjustin@gmail.com>
2017-03-11 21:44:17 -0500

    added functionality to mobileBurger

--- site/functions.js
+++ site/functions.js
@@ -1,1 +1,14 @@
+var burger = document.getElementById("mobileBurger");
+// var nav = document.getElementById("mobileNav");
 
+burger.onclick = burgerOpen;
+
+function burgerOpen() {
+	if (burger.className !== "isOpen") {
+		burger.className += "isOpen";
+		// nav.className += "isOpen";
+	} else {
+		burger.classList.remove("isOpen");
+		// nav.classList.remove("isOpen");
+	}
+}
