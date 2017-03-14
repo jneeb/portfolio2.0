41cda02a5ec2c2f5f0be1094f20f5b699ec4d612
jneeb <neebjustin@gmail.com>
2017-03-13 23:27:07 -0400

    removed global variables from functions.js

--- site/functions.js
+++ site/functions.js
@@ -1,19 +1,21 @@
-var burger = document.getElementById("mobileBurger");
-var nav = document.getElementById("mobileNav");
+(function() {
+	var burger = document.getElementById("mobileBurger");
+	var nav = document.getElementById("mobileNav");
 
-burger.onclick = navFunction;
+	burger.onclick = navFunction;
 
-function navFunction() {
-	if (burger.className !== "isOpen") {
-		burger.className += "isOpen";
-		nav.className += "isOpen";
-	} else {
-		burger.classList.remove("isOpen");
-		nav.classList.remove("isOpen");
+	function navFunction() {
+		if (burger.className !== "isOpen") {
+			burger.className += "isOpen";
+			nav.className += "isOpen";
+		} else {
+			burger.classList.remove("isOpen");
+			nav.classList.remove("isOpen");
+		}
 	}
-}
+}());
 
-window.onscroll = function() {
+	window.onscroll = function() {
 	var header = document.getElementById("desktopNav");
 
 	if (window.pageYOffset >= window.innerHeight && header.className !== "onScroll") {
