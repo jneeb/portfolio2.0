376e31c5cfa05c6e1c2754473097b9704cfd4923
jneeb <neebjustin@gmail.com>
2017-03-17 16:17:24 -0400

    got a functional contact form

--- site/contact/contactForm.php
+++ site/contact/contactForm.php
@@ -1,13 +1,46 @@
 <?php
-$myemail = "neebjustin@gmail.com";
-$name = check_input($_POST["name"]);
-$email = check_input($_POST["email"]);
-$comments = check_input($_POST["comments"]);
+$myemail = 'neebjustin@gmail.com';
+$name = check_input($_POST['name'], "Enter your name.");
+$email = check_input($_POST['email']);
+$comments = check_input($_POST['comments']);
 $subject = "Freelance Work";
+
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+    show_error("E-mail address not valid");
+}
+
 $message = "Name: $name
 Email: $email
 Message: $comments";
 
 mail($myemail, $subject, $message);
-header("Location: ./index.html");
+header('Location: thanks.html');
+exit();
+
+function check_input($data, $problem='')
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0)
+    {
+        show_error($problem);
+    }
+    return $data;
+}
+function show_error($myError)
+{
+?>
+    <html>
+    <body>
+
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
+
+    </body>
+    </html>
+<?php
+exit();
+}
 ?>
