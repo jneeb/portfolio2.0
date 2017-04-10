16ac4c3a8f576ce658f7e5191bc7492154eaa04e
jneeb <neebjustin@gmail.com>
2017-04-09 21:43:01 -0400

    added google anaylitics

--- site/contact/index.php
+++ site/contact/index.php
@@ -69,6 +69,15 @@ if (verifyFormToken('form1')) {
 		<link rel="icon" href="/img/favicon.ico">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600|Neuton|Pacifico" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-94009407-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</head>
 	<?php
 	$newToken = generateFormToken('form1');
