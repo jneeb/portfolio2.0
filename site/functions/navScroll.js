e85e9ee8781f0ad8337225bb3e3e70e7dda245df
jneeb <neebjustin@gmail.com>
2017-04-21 20:38:23 -0400

    added new desktop nav jquery

--- site/functions/navScroll.js
+++ site/functions/navScroll.js
@@ -4,8 +4,14 @@
 		var header = document.getElementById("desktopNav");
 		if (window.pageYOffset >= window.innerHeight && header.className !== "onScroll") {
 			header.className += "onScroll";
+			$("#desktopNav ul a").addClass("onScroll");
+			$("#desktopNav .logo").addClass("onScroll");
+			$(".socialLinks a").addClass("onScroll");
 		} else if (window.pageYOffset < window.innerHeight && header.className == "onScroll") {
 			header.classList.remove("onScroll");
+			$("#desktopNav ul a").removeClass("onScroll");
+			$("#desktopNav .logo").removeClass("onScroll");
+			$(".socialLinks a").removeClass("onScroll");
 		}
 	}
 }());
