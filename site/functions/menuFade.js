fc3d3485a1765c9e7bfeecc0f4a51dc8bb994a68
jneeb <neebjustin@gmail.com>
2017-05-02 11:04:39 -0400

    added menuFade to vorona menu

--- site/functions/menuFade.js
+++ site/functions/menuFade.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
-	$(".menuSelect").click(function () {
-		$(".menuSelect").removeClass("active");
-		$(this).addClass("active");
+	$("#lunch").click(function () {
+		$(".menu").fadeTo(800, 0);
+		$("#lunchMenu").fadeTo(800, 1);
+		$(".active").removeClass("active");
+		$("#lunch").addClass("active");
 	});
-});
-
-$(document).ready(function () {
-	$(".lunch").click(function () {
-		$(".menuFade").fadeOut("slow");
-		$(".lunchMenu").addClass("active");
+	$("#breakfast").click(function () {
+		$(".menu").fadeTo(800, 0);
+		$("#breakfastMenu").fadeTo(800, 1);
+		$(".active").removeClass("active");
+		$("#breakfast").addClass("active");
+	});
+	$("#dinner").click(function () {
+		$(".menu").fadeTo(800, 0);
+		$("#dinnerMenu").fadeTo(800, 1);
+		$(".active").removeClass("active");
+		$("#dinner").addClass("active");
 	});
 });
